71b27ba303b79f7af8121552f0dbe224b5538cb0
Kyle Freeman <austin.freeman-2@selu.edu>
2021-09-28 17:48:22 -0500

    Placeholder loading icon

---
+++ mcBackend/mcReact/src/Components/Body/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import UploadBtn from '../Buttons/UploadBtn'
+
+const Body = () => {
+    return (
+        <div>
+            <h3>Upload Micro-Organism</h3>
+            <UploadBtn />
+        </div>
+    )
+}
+
+export default Body
--- mcBackend/mcReact/src/Pages/App.js
+++ mcBackend/mcReact/src/Pages/App.js
@@ -1,24 +1,16 @@
-import { useState, useEffect } from 'react';
 import './App.css';
-import Header from '../Components/Header';
-import Body from '../Components/Body';
-import Logo from '../Components/Logo';
+import Header from '../Components/Header/Header';
+import Body from '../Components/Body/Body';
+import Logo from '../Components/Logo/Logo';
+import Loading from '../Components/Loading/Loading'
 
 function App() {
-  const [data, setData] = useState("")
-
-  useEffect(() => {
-    fetch("http://localhost:3001/home")
-    .then(res => res.text())
-    .then(data => setData(data))
-  }, [])
-
   return (
     <div className="Home">
       <Logo/>
       <Header/>
       <Body/>
-      <p>{data}</p>
+      <Loading/>
     </div>
   );
 }
